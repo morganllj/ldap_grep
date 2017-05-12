477f7783078889658bc0df23462fca946b27b10b
Morgan Jones <morgan@morganjones.org>
2017-05-12 18:07:01 -0400

    removed summary functionality, moving to separate script. https://en.wikipedia.org/wiki/Unix_philosophy

--- ldap_grep.pl
+++ ldap_grep.pl
@@ -9,30 +9,33 @@ sub print_usage();
 
 $,=$\="\n";
 
-my $argv_neg2 = $ARGV[-2];
-my $argv_neg1 = $ARGV[-1];
-my $argv_count = $#ARGV;
+my @arg = @ARGV;
 
 my $found=0;
-
 my $in;
-
 my $value;
 
 my %opts;
-getopt('is', \%opts);
+getopt('i', \%opts);
 
-if ($argv_count > 0 && ($argv_neg2 !~ /^\-/ || exists $opts{s})) {
-    open ($in, $argv_neg1) || die "can't open $argv_neg1";
-    $value = $argv_neg2 if ($argv_neg2 !~ /^\-/);
-} else {
-    $in = "STDIN";
-    $value = $argv_neg1;
+my $infile;
+for my $a (@arg) {
+    if ($a !~ /^-/) {
+	if (defined $value) {
+	    $infile = $a;
+	} else {
+	    $value = $a;
+	}
+    }
 }
 
+if (defined $infile) {
+    open ($in, $infile) || die "can't open $infile";
+} else {
+    $in = "STDIN";
+}
 
 my %conns;
-my %conns_summary;
 my %matching_conns;
 my $conn_from;
 my $search;
@@ -59,74 +62,16 @@ while (<$in>) {
     }
 
     if (exists $matching_conns{$conn}) {
-	if (exists $opts{s}) {
-	    for (@{$conns{$conn}}) {
-		if (/connection from (\d+\.\d+\.\d+\.\d+) /) {
-		    push @{$conns_summary{$conn}}, $1
-		}
-
-		if ((/(SRCH) base="[^"]*" scope=\d+ filter="([^"]+)"/) ||
-		    (/(MOD) dn="([^"]+)"/) ||
-		    (/(BIND) dn="([^"]+)"/)) {
-		    if (!exists $conns_summary{$conn}) {
-			push @{$conns_summary{$conn}}, "no_connection_info";
-		    }
-		    push @{$conns_summary{$conn}}, $1 . $2
-		}
-
-	    }
-	} 
-	if (!exists $opts{s} || (exists $opts{s} && defined $value)) {
-	    # the idea is we don't want to print if we're generating a summary of all log entries
-	    print @{$conns{$conn}};
-	}
-
+	print @{$conns{$conn}};
 	delete $conns{$conn}
     }
 }
 
 
 
-
-if (exists $opts{s}) {
-    print "\n***Summary:\n";
-    $,="";
-
-    my %summary;
-    
-    for my $c (keys %conns_summary) {
-	my $ip = shift @{$conns_summary{$c}};
-	push @{$summary{$ip}}, @{$conns_summary{$c}}
-    }
-
-    for my $ip (keys %summary) {
-	my %unique_summary_values;
-	for my $summary_value (@{$summary{$ip}}) {
-	    $unique_summary_values{$summary_value} = 1;
-	}
-	@{$summary{$ip}} = sort keys %unique_summary_values;
-    }
-
-    for my $ip (keys %summary) {
-	if (defined $summary{$ip}) {
-	    print $ip, " ", join (' ', @{$summary{$ip}});
-	} else {
-	    print $ip . "\n";
-	}
-    }
-
-}
-
-
-
-
-
-
-
 sub print_usage() {
-    print "\nusage: $0 [-i] PATTERN [file]\n";
-    print "or: $0 -s\n";
+    print "\nusage: $0 [-i] PATTERN file\n";
+    print "\tor: cat file | $0 [-i] PATTERN\n";
     print "\n";
     exit;
 }
-
